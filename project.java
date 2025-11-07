f92d1ff1a17042781f359c338147f0f87d937313
miguelmjq <mdjmirabalq@gmail.com>
2025-11-07 19:27:56 +0000

    started 11/7/25

--- project.java
+++ project.java
@@ -1,1 +1,16 @@
+class project {
+    public static void main(String[] args) {
 
+    }
+
+    public static boolean check(String input) {
+        int b, n = 0;
+        String c = input.substring(0, 1);
+        if (c.equals(")") || c.equals("]") || c.equals("}")) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
