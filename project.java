f9105e6027e0112747cc6f7ee9489b5dc37fe8a6
miguelmjq <mdjmirabalq@gmail.com>
2025-11-10 19:22:16 +0000

    11/10/25

--- project.java
+++ project.java
@@ -1,14 +1,25 @@
 class project {
     public static void main(String[] args) {
-
+            System.out.println(check("]()[]"));
     }
 
     public static boolean check(String input) {
+        
         int b, n = 0;
-        String c = input.substring(0, 1);
-        if (c.equals(")") || c.equals("]") || c.equals("}")) {
+        char [] m= input.toCharArray();
+        String c="";
+        { //check for instant false
+        c = input.substring(input.length()-1, input.length() );
+        if (c.equals("(") || c.equals("[") || c.equals("{")) {
             return false;
         }
+        c = input.substring(0, 1);
+        if (c.equals(")") || c.equals("]") || c.equals("}")) {
+           return false;
+        }
+    }
+
+    
 
         return true;
     }
